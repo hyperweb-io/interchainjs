ed34d91615d4eaca4dc2969abd044c7bda80d264
Zetazzz <zetazz@163.com>
2025-05-19 08:23:25 +0800

    add meta files

--- scripts/copy_readmes.sh
+++ scripts/copy_readmes.sh
@@ -11,11 +11,11 @@ mkdir -p "${DOCS_DIR}"
 find "${PROJECT_ROOT}/libs" "${PROJECT_ROOT}/packages" "${PROJECT_ROOT}/networks" -type f -name "README.md" ! -path "*/dist/*" ! -path "*/node_modules/*" | while read file; do
     target_dir="${DOCS_DIR}/$(dirname "${file#${PROJECT_ROOT}/}")"
     mkdir -p "${target_dir}"
-    cp "${file}" "${target_dir}/README.mdx"
-    echo "README.md file copied to docs/$(dirname "${file#${PROJECT_ROOT}/}")/README.mdx"
+    cp "${file}" "${target_dir}/index.mdx"
+    echo "README.md file copied to docs/$(dirname "${file#${PROJECT_ROOT}/}")/index.mdx"
 done
 
 # Copy root README.md
-cp "${PROJECT_ROOT}/README.md" "${DOCS_DIR}/README.mdx"
+cp "${PROJECT_ROOT}/README.md" "${DOCS_DIR}/index.mdx"
 
-echo "README.md files copied and renamed to README.mdx successfully!"
+echo "README.md files copied and renamed to index.mdx successfully!"
