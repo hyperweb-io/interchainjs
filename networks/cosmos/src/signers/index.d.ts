4bdbda78659615c5b84e8574be050a43a8906b0a
Zetazzz <zetazz@163.com>
2025-07-06 12:46:25 +0800

    remove unused files

--- networks/cosmos/src/signers/index.d.ts
+++
@@ -1,6 +0,0 @@
-export * from './types';
-export * from './base-signer';
-export * from './direct-signer';
-export * from './amino-signer';
-export * from './simple-wallet';
-export { ICosmosSigner, CosmosSignArgs, CosmosAccount, CosmosMessage, EncodedMessage, CosmosSignOptions } from '../src/workflows/types';
